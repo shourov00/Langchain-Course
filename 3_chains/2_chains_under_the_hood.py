17cbc45e87b68141126b5a2da5f2876a7c17a2ff
Fazley Rabbi <sfazleyrabbi@gmail.com>
2025-03-15 13:29:36 +0600

    Added extended and parallel chains to run multiple prompts together

--- 3_chains/2_chains_under_the_hood.py
+++ 3_chains/2_chains_under_the_hood.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnableLambda
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 
@@ -21,15 +20,16 @@ prompt_template = ChatPromptTemplate.from_messages(
     ]
 )
 
-# Define additional processing steps using RunnableLambda
-uppercase_output = RunnableLambda(lambda x: x.upper())
-count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+# Create individual runnables (steps in the chain)
+format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
+invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
+parse_output = RunnableLambda(lambda x: x.content)
 
-# Create the combined chain using LangChain Expression Language (LCEL)
-chain = prompt_template | model | StrOutputParser() | uppercase_output | count_words
+# Create the RunnableSequence (equivalent to the LCEL chain)
+chain = RunnableSequence(first=format_prompt, middle=[invoke_model], last=parse_output)
 
 # Run the chain
-result = chain.invoke({"topic": "lawyers", "joke_count": 3})
+response = chain.invoke({"topic": "lawyers", "joke_count": 3})
 
 # Output
-print(result)
+print(response)
