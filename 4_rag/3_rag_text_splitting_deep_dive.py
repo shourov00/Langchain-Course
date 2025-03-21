9dacef8d0a537c87a5768e23bc0967a555154c5a
Fazley Rabbi <sfazleyrabbi@gmail.com>
2025-03-21 12:07:09 +0600

    Add new RAG embedding and conversational implementations with Chroma

--- 4_rag/3_rag_text_splitting_deep_dive.py
+++ 4_rag/3_rag_text_splitting_deep_dive.py
@@ -9,7 +9,7 @@ from langchain.text_splitter import (
     TokenTextSplitter,
 )
 from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
